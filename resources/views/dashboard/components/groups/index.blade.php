a92db1a404ae7160723aebcae3fcc104677e81b5
Ollie Reardon <ojreardon@gmail.com>
2019-06-30 13:33:48 +0100

    Removed important flag from floating badges

--- resources/views/dashboard/components/groups/index.blade.php
+++ resources/views/dashboard/components/groups/index.blade.php
@@ -24,7 +24,7 @@
                             <span class="drag-handle"><i class="ion ion-drag"></i></span>
                             @endif
                             {{ $group->name }}
-                            <span class="badge badge-info">{{ $group->components->count() }}</span>
+                            <span class="badge badge-info float-none">{{ $group->components->count() }}</span>
                         </h4>
                     </div>
                     <div class="col-6 text-right">
